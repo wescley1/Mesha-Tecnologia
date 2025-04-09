e7bfb3294f1f5a72341f0ef70208d9b33265ed8f
wescley1 <wescleydecarvalho@hotmail.com>
2025-04-08 21:10:15 -0300

    new requirement
    adicionado requirement webdriver-manager para que a automaçao funcione em ambos OS

--- gerenciador_vendas/automacao_vendas.py
+++ gerenciador_vendas/automacao_vendas.py
@@ -3,9 +3,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # Caminho do WebDriver (ajuste se necessário)
-driver = webdriver.Chrome()
+#driver = webdriver.Chrome()
 
 # Carrega as vendas do JSON
 with open('vendas.json', 'r', encoding='utf-8') as f:
